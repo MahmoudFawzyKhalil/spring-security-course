754d085fb37b8cdeb1e1cc3167e72ac09833e349
Omar-Abdelmutaleb <omar197mohamed@gmail.com>
2024-05-07 23:59:29 +0200

    first commit

--- ss/src/main/java/com/example/security/apikey/ApiKeyAuthenticationConfigurer.java
+++ ss/src/main/java/com/example/security/apikey/ApiKeyAuthenticationConfigurer.java
@@ -19,7 +19,7 @@ public class ApiKeyAuthenticationConfigurer extends AbstractHttpConfigurer<ApiKe
         http.authenticationProvider(apiKeyAuthenticationProvider);
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
 
-        ApiKeyAuthenticationFilter apiKeyAuthenticationFilter = new ApiKeyAuthenticationFilter(authenticationManager);
+        ApiKeyAuthenticationFilter2 apiKeyAuthenticationFilter = new ApiKeyAuthenticationFilter2(authenticationManager, new ApiKeyAuthenticationConverter());
         http.addFilterBefore(apiKeyAuthenticationFilter, BasicAuthenticationFilter.class);
     }
 }
