1178809e7b5b3a880bf7ed10804dfebb31ec0183
Mohamed Adel <mohamedadelfarah@gmail.com>
2024-05-08 15:40:32 +0300

    Add custom ApiKeyAuthenticationFilter

--- ss/src/main/java/com/example/security/apikey/ApiKeyAuthenticationConfigurer.java
+++ ss/src/main/java/com/example/security/apikey/ApiKeyAuthenticationConfigurer.java
@@ -19,7 +19,7 @@ public class ApiKeyAuthenticationConfigurer extends AbstractHttpConfigurer<ApiKe
         http.authenticationProvider(apiKeyAuthenticationProvider);
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
 
-        ApiKeyAuthenticationFilter apiKeyAuthenticationFilter = new ApiKeyAuthenticationFilter(authenticationManager);
+        CustomApiKeyAuthenticationFilter apiKeyAuthenticationFilter = new CustomApiKeyAuthenticationFilter(authenticationManager, new ApiKeyAuthenticationConverter());
         http.addFilterBefore(apiKeyAuthenticationFilter, BasicAuthenticationFilter.class);
     }
 }
